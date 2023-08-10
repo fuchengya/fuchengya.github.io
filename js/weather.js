2e154ea1080049de9d6247db603ea20faa327aaf
fucheng9 <3306851358@qq.com>
2023-08-10 15:58:58 +0800

    Site updated: 2023-08-10 15:58:55

---
+++ js/weather.js
@@ -0,0 +1,12 @@
+// WIDGET = {
+//     "CONFIG": {
+//         "layout": "1",
+//         "width": "300",
+//         "height": "100",
+//         "background": "1",
+//         "dataColor": "FFFFFF",
+//         "borderRadius": "5",
+//         "modules": "01",
+//         "key": "41e1ab59bba1464f85063a94b38aadd0"
+//     }
+// }
